5bf1ff63e8de3b968bfa7c4bdbe54b010f5655e0
Danish <danish8802204230@gmail.com>
2017-09-08 11:29:41 +0530

    Fixed internal error for some site and sent to the callback

--- index.js
+++ index.js
@@ -6,41 +6,41 @@ module.exports = {
 	fetch: function (uri, user_options, callback) {
 		var options = {
 			url: uri,
-            timeout: 5000,
+			timeout: 5000,
 			headers: {
 				'User-Agent': 'request'
 			}
 		};
-		
+
 		//  setup the args/user_options
 		var user_args = [];
 		for (var i = 0; i < arguments.length; i++) {
 			user_args.push(arguments[i]);
 		}
-		
+
 		// remove these from arg array
 		uri = user_args.shift();
 		callback = user_args.pop();
-		
+
 		// get user_options if specified
-		if(user_args.length > 0){
+		if (user_args.length > 0) {
 			user_options = user_args.shift();
-		}else{
+		} else {
 			user_options = null;
 		}
 
 		// override default headers
-		if(user_options){
+		if (user_options) {
 			options.headers = user_options.headers;
 		}
-		
+
 		request.get(options, function (error, response, body) {
-			if (!error && response.statusCode === 200){
+			if (!error && response.statusCode === 200) {
 				var $ = cheerio.load(body);
 				var meta = $('meta');
 				var keys = Object.keys(meta);
 				var meta_obj = {};
-				keys.forEach(function (key){
+				keys.forEach(function (key) {
 					if (meta[key].attribs != undefined) {
 						if (meta[key].attribs.property && meta[key].attribs.content) {
 							meta_obj[meta[key].attribs.property] = meta[key].attribs.content;
@@ -50,14 +50,18 @@ module.exports = {
 						}
 					}
 				});
-				
+
 				callback(null, meta_obj);
-			}else{
-                if(typeof response.statusCode !== 'undefined'){
-                    callback('Response code: ' + response.statusCode, null);
-                }else{
-                    callback(error, null);
-                }
+			} else {
+				if (request) {
+					if (typeof response.statusCode !== 'undefined') {
+						callback('Response code: ' + response.statusCode, null);
+					} else {
+						callback(error, null);
+					}
+				} else {
+					callback(error, null)
+				}
 			}
 		});
 	}
